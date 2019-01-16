7ae051b67c894ef0f4903126538a3ecf2e1aef76
Ivan Guba <ekl17@mail.ru>
2019-01-16 16:28:09 +0400

    hw4 gcp vm instance creation

---
+++ packer-base/config-scripts/install_mongodb.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Добавляем ключи и реозиторий
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+
+# Обновляем пакеты и ставим Mongo
+
+sudo apt update
+sudo apt install -y mongodb-org
+
+# Стартуем Mongo и добавляем в автозапуск
+
+sudo systemctl start mongod
+sudo systemctl enable mongod
---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# Install ruby
+
+apt update
+apt install -y ruby-full ruby-bundler build-essential
